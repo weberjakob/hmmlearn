8956e9384e6e18ec772b6429b7904791bddc4de8
Jakob Weber <weberjakob@gmx.at>
2023-08-06 22:32:19 +0200

    first commit

---
+++ examples/plot_finance.py
@@ -0,0 +1,24 @@
+import collections
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gs
+import scipy.stats
+import yfinance as yf
+
+from sklearn.utils import check_random_state
+
+from hmmlearn import hmm
+import matplotlib
+
+bvb = yf.Ticker('BVB.DE')
+
+print(bvb.info)
+print(bvb.history(period="max"))
+
+
+
+
+
+
+
+gen_model = hmm.GaussianHMM(n_components=4, covariance_type="full")
